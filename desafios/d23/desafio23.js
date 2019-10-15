ac15774505bafbaf9123cfc71334a921ad279306
Rodrigo <rodrigo.talison@wetoksoft.com.br>
2019-10-15 18:02:02 -0300

    Desafio 23 incompleto

--- desafios/d23/desafio23.js
+++ desafios/d23/desafio23.js
@@ -1,19 +1,54 @@
   (function(w,d){ 
     'use strict'
 
+    var $input = d.querySelector('[data-js="resultValue"]');
+
+    //buttons
+    var $btn0 = d.querySelector('[data-number-js="0"]');
+    var $btn1 = d.querySelector('[data-number-js="1"]');
+    var $btn2 = d.querySelector('[data-number-js="2"]');
+    var $btn3 = d.querySelector('[data-number-js="3"]');
+    var $btn4 = d.querySelector('[data-number-js="4"]');
+    var $btn5 = d.querySelector('[data-number-js="5"]');
+    var $btn6 = d.querySelector('[data-number-js="6"]');
+    var $btn7 = d.querySelector('[data-number-js="7"]');
+    var $btn8 = d.querySelector('[data-number-js="8"]');
+    var $btn9 = d.querySelector('[data-number-js="9"]');
+
+    var $buttonsListCount = document.querySelectorAll('[data-number-js]').length;
+	
+	function callBack()
+	{		
+		alert(this.value);
+	}
+
+	for (var i = 0; i <= $buttonsListCount - 1; i++) 
+	{
+
+		var $btn = d.querySelector('[data-number-js="'+ i +'"]');	
+
+		$btn.addEventListener('click', callBack);
+	}
+    
+
    /*
 Vamos desenvolver mais um projeto. A ideia é fazer uma mini-calculadora.
 As regras são:
 - Deve ter somente 1 input, mas não deve ser possível entrar dados nesse input
 diretamente;
 - O input deve iniciar com valor zero;
+
 - Deve haver 10 botões para os números de 0 a 9. Cada botão deve ser um número;
+
 - Deve haver 4 botões para as operações principais: soma (+), subtração(-),
 multiplicação(x) e divisão(÷);
+
 - Deve haver um botão de "igual" (=) que irá calcular os valores e um botão "CE"
 que irá limpar o input, deixando-o com valor 0;
+
 - A cada número pressionado, o input deve atualizar concatenando cada valor
 digitado, como em uma calculadora real;
+
 - Ao pressionar um botão com uma das 4 operações, deve aparecer o símbolo da
 operação no input. Se o último caractere no input já for um símbolo de alguma
 operação, esse caractere deve ser substituído pelo último pressionado.
